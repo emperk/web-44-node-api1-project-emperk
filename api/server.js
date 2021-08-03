1c593db0b574312fac862b3cb3b01a0980c7d173
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-08-02 19:36:25 -0700

    adjusted delete endpoint

--- api/server.js
+++ api/server.js
@@ -6,15 +6,23 @@ const server = express();
 server.use(express.json())
 
 server.delete('/api/users/:id', async (req, res) => {
-  const possibleUser = await User.findById(req.params.id)
-  if (!possibleUser) {
-    res.status(404).json({
-      message: 'The user with the specified ID does not exist'
+  try {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+      res.status(404).json({
+        message: 'The user with the specified ID does not exist'
+      })
+    } else {
+      const deletedUser = await User.remove(possibleUser.id)
+      console.log(deletedUser)
+      res.status(200).json(deletedUser)
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'error with deleting the user',
+      err: err.message,
+      stack: err.stack,
     })
-  } else {
-    const deletedUser = await User.remove(possibleUser.id)
-    console.log(deletedUser)
-    res.status(200).json(deletedUser)
   }
 })
 
